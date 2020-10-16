0cade50d961950c16614a8fe8f6e4302dd6eead1
antonija2811 <agizdi00@gmail.com>
2020-10-16 16:01:52 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
-import Home from './components/Home';
+import Home from './components/Home/Home';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
 import Default from './components/Default';
